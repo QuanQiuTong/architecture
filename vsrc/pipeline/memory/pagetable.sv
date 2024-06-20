14a447472aa3d868116d102af7058e01e5f24188
QuanQiuTong <fqt1550887523@outlook.com>
2024-06-20 23:15:36 +0800

    fix combinational loop

--- vsrc/pipeline/memory/pagetable.sv
+++ vsrc/pipeline/memory/pagetable.sv
@@ -15,57 +15,63 @@ module translate (
     output logic valid,              // 地址翻译是否有效
     output logic [63:0] mem_addr,    // 内存访问地址
     output logic mem_req,            // 内存请求信号
-    input logic [63:0] pte,          // 内存返回数据
-    input logic pte_valid,           // 内存数据有效信号
+    input  [63:0] pte,          // 内存返回数据
+    input  pte_valid,           // 内存数据有效信号
     output logic done                // 地址翻译完成信号
 );
-    wire bare = !en || satp[63:60] == 0 || mmode == 'b11;
-    wire got = pte_valid && state == READ_PTE && pte[0] == 1 && pte[3:1] != 'b000;
+    wire bare = satp[63:60] == 0 || mmode == 'b11;
 
-    enum reg [1:0] {
-        IDLE,
-        READ_PTE,
-        DONE
-    } state, next_state;
+    enum reg [1:0] { IDLE, READ_PTE, DONE } state, next_state;
     reg [43:0] ppn, next_ppn;
     reg [1:0] level, next_level;
+    reg [63:0] phy_addr, next_pa;
 
-    always_ff @(posedge clk) begin
-        state <= reset ? IDLE : next_state;
+    always_ff @(posedge clk) 
+    if (reset) begin
+        state <= IDLE;
+        level <= 0;
+        ppn <= 0;
+        phy_addr <= 0;
+    end else begin
+        state <= next_state;
         level <= next_level;
         ppn <= next_ppn;
+        phy_addr <= next_pa;
     end
 
     always_comb begin
         next_state = state;
         next_level = level;
         next_ppn = ppn;
+        next_pa = phy_addr;
         case (state)
-            IDLE:
-                if (bare) begin
-                    next_state = IDLE;
-                end else if (satp[63:60] == 8) begin
-                    next_ppn = satp[43:0];
-                    next_level = 2;
-                    next_state = READ_PTE;
-                end
+        IDLE:
+            if (!en || bare) begin
+                next_state = IDLE;
+            end else if (satp[63:60] == 8) begin
+                next_ppn = satp[43:0];
+                next_level = 2;
+                next_state = READ_PTE;
+            end
 
-            READ_PTE:
-                if (pte_valid) begin
-                    if (pte[0] == 0) begin // Invalid PTE
-                        next_state = DONE;
-                    end else if (pte[3:1] != 'b000) begin // Leaf PTE
-                        next_state = DONE;
-                    end else begin
-                        next_ppn = pte[53:10];
-                        next_state = (level == 0) ? DONE : READ_PTE;
-                        next_level = level - 1;
-                    end
+        READ_PTE:
+            if (pte_valid) begin
+                if (pte[0] == 0) begin // Invalid PTE
+                    next_state = DONE;
+                end else if (pte[3:1] != 'b000) begin // Leaf PTE
+                    next_state = DONE;
+                    next_pa = {8'b0, pte[53:10], va[11:0]};
+                end else begin
+                    next_ppn = pte[53:10];
+                    next_state = (level == 0) ? DONE : READ_PTE;
+                    next_level = level - 1;
                 end
+            end
 
-            DONE:
-                next_state = en ? DONE : IDLE;
-            default: begin end
+        DONE:
+            next_state = en ? DONE : IDLE;
+        default:
+            begin end
         endcase
     end
 
@@ -76,14 +82,8 @@ module translate (
         1: mem_addr = {8'b0, ppn, va[29:21], 3'b000};
         0: mem_addr = {8'b0, ppn, va[20:12], 3'b000};
     endcase
-    assign done = bare || state == DONE || got;
-
-    reg [63:0] _pa;
-    always_latch
-        if (got)
-            _pa = {8'b0, pte[53:10], va[11:0]};
-
-    assign pa = bare ? va : _pa;
+    assign done = !en || bare || state == DONE;            
+    assign pa = bare ? va : phy_addr;
     
     assign valid = 1; // ignore page fault
 endmodule
